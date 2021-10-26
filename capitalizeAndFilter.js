8252c5fd25684d050ce2b7da452f3a417684ddb8
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-10-26 10:34:27 -0700

    cap and filter w for loop passing

--- capitalizeAndFilter.js
+++ capitalizeAndFilter.js
@@ -1,8 +1,11 @@
 const capitalizeAndFilter = (arrayOfStrings) => {
-
-  for(let i = 0 ; i < arrayOfStrings.length ; i++){
-    arrayOfStrings[i] = arrayOfStrings[i].charAt(0).toUpperCase() + arrayOfStrings[i].slice(i);
+ 
+  for (let i = 0 ; i < arrayOfStrings.length ; i++){
+    arrayOfStrings[i] = arrayOfStrings[i].charAt(0).toUpperCase() + arrayOfStrings[i].slice(1);
   }      
+
+  const filteredArray = arrayOfStrings.filter((word) => word.charAt(0) !== 'F');
+  return filteredArray;
 };
 module.exports = capitalizeAndFilter;
     
--- capitalizeAndFilter.test.js
+++ capitalizeAndFilter.test.js
@@ -2,8 +2,8 @@ const capitalizeAndFilter = require('./capitalizeAndFilter.js');
 
 describe ('capitalizeAndFilter', () => {
   it('Should capitalize each word and remove any that begin with "f"', () => {
-    const arrayOfStrings = ['friendly', 'apple', 'fiend', 'kiwi'];
-    capitalizeAndFilter(arrayOfStrings);
-    expect (arrayOfStrings).toEqual(['Apple', 'Kiwi']);
+    const arrayOfStrings = ['Friendly', 'apple', 'fiend', 'kiwi'];
+    const newFilter =  capitalizeAndFilter(arrayOfStrings);
+    expect (newFilter).toEqual(['Apple', 'Kiwi']);
   });
 });
